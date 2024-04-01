8bebebfec6d81ebb0372aacdb9400293bcf4edbe
TaurnilTelemnar <taurnil@live.ru>
2024-04-01 22:56:22 +0300

    ADD required quest

--- src/main/java/com/javarush/kojin/config/Config.java
+++ src/main/java/com/javarush/kojin/config/Config.java
@@ -104,6 +104,133 @@ public class Config {
         questionOne.getAnswers().add(answerOne);
         questionOne.getAnswers().add(answerTwo);
 
+
+        Quest task = Quest.builder()
+                .authorId(admin.getId())
+                .isDraft(false)
+                .questions(new ArrayList<>())
+                .name("Ынопланетный")
+                .build();
+        questService.createQuest(task);
+        admin.getUserQuests().add(task);
+
+        Question taskOne = Question.builder()
+                .name("Начало")
+                .gameState(GameState.PLAY)
+                .questId(task.getId())
+                .text("Ты потерял память. Принять вызов НЛО?")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskOne);
+        task.setStartQuestionId(taskOne.getId());
+
+
+        Question taskTwo = Question.builder()
+                .name("И что я тут забыл?!")
+                .gameState(GameState.LOSE)
+                .questId(task.getId())
+                .text("Ты отклонил вызов. Поражение.")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskTwo);
+
+        Question taskThree = Question.builder()
+                .name("Крымский мостик кЭпа")
+                .gameState(GameState.PLAY)
+                .questId(task.getId())
+                .text("Ты принял вызов. Поднимаешься на мостик к капитану?")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskThree);
+
+        Answer startOne = Answer.builder()
+                .text("Принять вызов")
+                .nextQuestionId(taskThree.getId())
+                .questionId(taskOne.getId())
+                .build();
+        Answer startTwo = Answer.builder()
+                .text("Отклонить вызов")
+                .nextQuestionId(taskTwo.getId())
+                .questionId(taskOne.getId())
+                .build();
+        answerService.createAnswer(startOne);
+        answerService.createAnswer(startTwo);
+
+        taskOne.getAnswers().add(startOne);
+        taskOne.getAnswers().add(startTwo);
+
+        Question taskFour = Question.builder()
+                .name("Не ходите дети в НЛО гулять")
+                .gameState(GameState.LOSE)
+                .questId(task.getId())
+                .text("Ты отказался подниматься на мостик. Поражение.")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskFour);
+
+        Question taskFive = Question.builder()
+                .name("Ты кто такой? Давай приседания!")
+                .gameState(GameState.PLAY)
+                .questId(task.getId())
+                .text("Ты поднялся на мостик. Ты кто?")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskFive);
+
+        Answer comeOnToMaineShishkaOne = Answer.builder()
+                .text("Подняться на мостик")
+                .nextQuestionId(taskFive.getId())
+                .questionId(taskThree.getId())
+                .build();
+
+        Answer comeOnToMaineShishkaTwo = Answer.builder()
+                .text("Отказаться подниматься на мостик")
+                .nextQuestionId(taskFour.getId())
+                .questionId(taskThree.getId())
+                .build();
+
+        answerService.createAnswer(comeOnToMaineShishkaOne);
+        answerService.createAnswer(comeOnToMaineShishkaTwo);
+
+        taskThree.getAnswers().add(comeOnToMaineShishkaOne);
+        taskThree.getAnswers().add(comeOnToMaineShishkaTwo);
+
+
+        Question taskSix = Question.builder()
+                .name("Не 3.14зди")
+                .gameState(GameState.LOSE)
+                .questId(task.getId())
+                .text("Твою ложь разоблачили. Поражение.")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskSix);
+
+        Question taskSeven = Question.builder()
+                .name("Возвращение блудного попугая")
+                .gameState(GameState.WIN)
+                .questId(task.getId())
+                .text("Тебя вернули домой. Победа.")
+                .answers(new ArrayList<>())
+                .build();
+        questionService.createQuestion(taskSeven);
+
+        Answer whoAreYouOne = Answer.builder()
+                .text("Рассказать о себе правду")
+                .nextQuestionId(taskSeven.getId())
+                .questionId(taskFive.getId())
+                .build();
+
+        Answer whoAreYouTwo = Answer.builder()
+                .text("Солгать о себе")
+                .nextQuestionId(taskSix.getId())
+                .questionId(taskFive.getId())
+                .build();
+
+        answerService.createAnswer(whoAreYouOne);
+        answerService.createAnswer(whoAreYouTwo);
+
+        taskFive.getAnswers().add(whoAreYouOne);
+        taskFive.getAnswers().add(whoAreYouTwo);
     }
 
 }
